eb752d3363636190be14e63ce576563777733454
DESKTOP-NO2GMU0\musign220210-2 <ahjeong921@gmail.com>
2023-03-16 17:46:56 +0900

    헤더 90%

--- js/main.js
+++ js/main.js
@@ -1,14 +1,87 @@
-$(document).ready(function(){
-        
+jQuery(function ($) {
     /* ======================================================================
     *
-    *  Load Functions
+    * Powered by MUSIGN  
+    * Version 1.0
+    * js/musign.js 
+    * reference: https://toyfight.co/
+    * 
+    ====================================================================== */
+
+    /* ======================================================================
+    *
+    *  RUN 
     * 
     ====================================================================== */
 
 
-    mouseMovement(0.08);
-    $('.sec01').addClass('ddd')
+    init(true);
+
+
+    /* ======================================================================
+    *
+    *  INIT 
+    * 
+    ====================================================================== */
+
+    function init(loadonce) {
+
+
+        // only MAIN
+        if ($('body').hasClass('home')) {}
+
+        // only ABOUT US
+        if ($('body').hasClass('about')) {}
+        
+
+        // First load
+        if (typeof loadonce !== 'undefined' && loadonce === true) {
+            // >=2nd load
+        } else {
+            initOnload();
+        }
+
+        $(window).trigger('resize');
+
+        mouseMovement(0.08);
+
+    }
+
+
+    /* ======================================================================
+     *
+     *  ON LOAD
+     * 
+     ====================================================================== */
+
+
+    // INITIALIZE LOAD
+    function initOnload() {}
+
+    $(window).on('load', function () {});
+
+
+    /* ======================================================================
+     *
+     *  ON RESIZE
+     * 
+     ====================================================================== */
+
+
+    // INITIALIZE RESIZE
+    function init_resize() {
+
+        // setTimeout to fix IOS animation on rotate issue
+        setTimeout(function () {
+
+            //
+
+        }, 400);
+
+    }
+
+    // Init resize on reisize
+    $(window).on('resize', init_resize);
 
 
     /* ======================================================================
@@ -24,17 +97,17 @@ $(document).ready(function(){
             var cursorX = e.pageX;
             var cursorY = e.pageY;
             var halfWindowWidth = window.innerWidth / 2;
-            var halfWindowHeight = $(window).height()/2;
+            var halfWindowHeight = $(window).height() / 2;
   
             var moveX = (cursorX - (halfWindowWidth))*-moveAmount;
             var moveY = (cursorY - (halfWindowHeight))*-moveAmount;
-            var rotateY = moveX*-0.09;
+            var rotateY = moveX * - 0.09;
 
             TweenMax.to($('.figure_mover .js_figure1'),
                 1.175, {
-                    x: moveX*0.4,
-                    y: moveY*0.8,
-                    rotationY: rotateY*0.4
+                    x: moveX * 0.4,
+                    y: moveY * 0.8,
+                    rotationY: rotateY * 0.4
                 }
             );
 
@@ -48,33 +121,43 @@ $(document).ready(function(){
         });
     } 
 
-
-    //플러그인 사용 없이 bg_mouseover
-    /*
-    let figures;
-    let x = 0;
-    let y = 0;
-    let mx = 0;
-    let my = 0;
-    let speed = 0.08;
-
-    window.onload = function(){
-        figures = document.getElementsByClassName("figures_img")[0];
-        window.addEventListener("mousemove", mouseFunc, false);
-        loop();
+    //data active
+    function dataActiveOff(e) {
+        e.attr('data-active', 'off');
     }
 
-    function mouseFunc(e){
-        x = (e.clientX - window.innerWidth / 2);
-        y = (e.clientY - window.innerHeight / 2);
-        
-        figures.style.transform = "translate("+ (mx/-15) +"px," + (my/-15) +"px)";
+    function dataActiveOn(e) {
+        e.attr('data-active', 'on');
     }
 
-    function loop(){
-        mx += (x - mx) * speed;
-        my += (y - my) * speed;
-        window.requestAnimationFrame(loop);
-    }*/
 
-})
+}); // End jQuery
+
+
+//플러그인 사용 없이 bg_mouseover
+/*
+let figures;
+let x = 0;
+let y = 0;
+let mx = 0;
+let my = 0;
+let speed = 0.08;
+
+window.onload = function(){
+    figures = document.getElementsByClassName("figures_img")[0];
+    window.addEventListener("mousemove", mouseFunc, false);
+    loop();
+}
+
+function mouseFunc(e){
+    x = (e.clientX - window.innerWidth / 2);
+    y = (e.clientY - window.innerHeight / 2);
+    
+    figures.style.transform = "translate("+ (mx/-15) +"px," + (my/-15) +"px)";
+}
+
+function loop(){
+    mx += (x - mx) * speed;
+    my += (y - my) * speed;
+    window.requestAnimationFrame(loop);
+}*/

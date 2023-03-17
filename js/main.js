349a02b0d23a96a1f8a5bdb20c8ce88f0ad2eafa
DESKTOP-NO2GMU0\musign220210-2 <ahjeong921@gmail.com>
2023-03-17 17:24:25 +0900

    sec01 작업 진행 중

--- js/main.js
+++ js/main.js
@@ -43,8 +43,17 @@ jQuery(function ($) {
 
         $(window).trigger('resize');
 
+        //mouseMove
         mouseMovement(0.08);
 
+        // smoothscroll 적용 시 사용  scrollYPos = (smoothScrollActive ? smoothScroll.vars.current : window.pageYOffset);
+
+        //titScroll
+        sectionFix();
+
+        //nav
+        navStart();
+        navAction();
     }
 
 
@@ -94,14 +103,14 @@ jQuery(function ($) {
     function mouseMovement(moveAmount) {
         $(".sec01").mousemove(function(e) {
 
-            var cursorX = e.pageX;
-            var cursorY = e.pageY;
-            var halfWindowWidth = window.innerWidth / 2;
-            var halfWindowHeight = $(window).height() / 2;
+            let cursorX = e.pageX;
+            let cursorY = e.pageY;
+            let halfWindowWidth = window.innerWidth / 2;
+            let halfWindowHeight = $(window).height() / 2;
   
-            var moveX = (cursorX - (halfWindowWidth))*-moveAmount;
-            var moveY = (cursorY - (halfWindowHeight))*-moveAmount;
-            var rotateY = moveX * - 0.09;
+            let moveX = (cursorX - (halfWindowWidth))*-moveAmount;
+            let moveY = (cursorY - (halfWindowHeight))*-moveAmount;
+            let rotateY = moveX * - 0.09;
 
             TweenMax.to($('.figure_mover .js_figure1'),
                 1.175, {
@@ -119,7 +128,84 @@ jQuery(function ($) {
                 }
             );
         });
-    } 
+    }
+
+    function scrollQuery(y) {
+
+        let windowHeight = $(window).height();
+        let jsFiguresWrap = $('.figure_mover');
+
+
+        if (y >= windowHeight*0.15) {
+            dataActiveOff(jsFiguresWrap);
+            dataActiveOff($('header nav'));
+            dataActiveOff($('.scroll_arrow_wrap'));
+        } else {
+            dataActiveOn(jsFiguresWrap);
+            dataActiveOn($('header nav'));
+            dataActiveOn($('.scroll_arrow_wrap'));
+        }
+    }
+
+    function sectionFix(){
+        let sec01 = document.querySelector(".sec01");
+
+        ScrollTrigger.create({
+            trigger: ".sec01",
+            scrub: true,
+            pin: true,
+            start: "top top",
+            end: () => `+=${sec01.offsetHeight}`
+        });
+    }
+
+    function navStart(){
+        let fadeNav = document.querySelectorAll('nav li')
+
+        fadeNav.forEach(function(textanimation,index){
+            gsap.to(textanimation,1,{
+                delay:(index + 1) * .15,
+                opacity:1,
+                y : 0
+            })
+        })
+    }
+
+    function navAction(){
+        $(window).scroll(function(){
+            var sTop = $(window).scrollTop();
+
+            if(sTop >= 100){
+                dataActiveOn($('header nav'));
+            }
+
+            if(sTop >= 200){
+                dataActiveOff($('.figure_mover'));
+                dataActiveOff($('.page_tit'));
+                dataActiveOff($('.page_bg'));
+                dataActiveOff($('.sec01'));
+            }
+
+            else{
+                dataActiveOff($('header nav'));
+                dataActiveOn($('.figure_mover'));
+                dataActiveOn($('.page_tit'));
+                dataActiveOn($('.page_bg'));
+                dataActiveOn($('.sec01'));
+            }
+
+            if($('header nav').data('active') != 'on'){
+                let navDataOn = document.querySelectorAll('nav li')
+
+                navDataOn.forEach(function(textanimation,index){
+                    gsap.to(textanimation,1,{
+                        delay:(index + 1) * .15,
+                        y : -100
+                    })
+                })
+            }
+        })
+    }
 
     //data active
     function dataActiveOff(e) {
